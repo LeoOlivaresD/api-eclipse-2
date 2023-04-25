6b9588030139851326e62a4b11916e427f531ca9
LeoOlivaresD <cspraword@gmail.com>
2023-04-25 02:23:51 -0400

    commit inicial

---
+++ src/main/java/cl/leo/apieclipse/ApiEclipseApplication.java
@@ -0,0 +1,13 @@
+package cl.leo.apieclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEclipseApplication.class, args);
+	}
+
+}
